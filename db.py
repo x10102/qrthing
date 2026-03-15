b62261da87c5c57460d8db30ab5e6b460aa5efc0
x10102 <x10102.ot@gmail.com>
2026-03-16 00:28:14 +0100

    Might have copied too much code lmao

--- db.py
+++ db.py
@@ -9,34 +9,12 @@ class BaseModel(Model):
 # type: ignore
 class User(BaseModel):
     id = AutoField()
-    discord = TextField(null=True)
-    display_name = TextField(null=True)
-    nickname = TextField(unique=True)
+    username = TextField(unique=True)
     password = BlobField(null=True)
-    temp_pw = BooleanField(default=True, null=True)
-    wikidot = TextField(unique=True)
-    avatar_hash = TextField(default=True, null=True)
-
-    @property
-    def can_login(self) -> bool:
-        return self.password != None
     
     # Flask-Login stuff
     is_anonymous = False
     is_active = True
-    
-    @property
-    def is_authenticated(self) -> bool:
-        return len(self.password) > 0
-
-    def to_dict(self) -> dict:
-        return {
-        'id': self.id,
-        'nickname': self.nickname,
-        'wikidot': self.wikidot,
-        'discord': self.discord,
-        'displayName': self.display_name
-    }
 
     class Meta:
         table_name = 'User'
